952f3a6e57baf8f514302f4fd596e7b347c179ab
antongiulio <anto.calab@gmail.com>
2014-08-06 16:48:56 +0200

    level 2

---
+++ Assets/Script/StartMenuGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuGUI : MonoBehaviour {
+
+	
+	void Update () {
+
+	}
+
+
+	void OnGUI(){
+
+		if(GUI.Button(new Rect(Screen.width/2 - 50,150,100,20),"Gioco Uno")){
+			Application.LoadLevel("Scene1");
+		}
+
+		if(GUI.Button(new Rect(Screen.width/2 - 50,190,100,20),"Gioco Due")){
+
+		}
+	}
+}
