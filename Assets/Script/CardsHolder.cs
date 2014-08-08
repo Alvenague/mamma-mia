5be851b13f0f1b1bd2094b287eef6882eb357388
Antongiulio Calabria <anto.calab@gmail.com>
2014-08-08 12:25:44 +0200

    shuffle
    problemi col movimento delle carte

---
+++ Assets/Script/CardsHolder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardsHolder : MonoBehaviour {
+
+	public GameObject[] cards;
+
+	// Use this for initialization
+	void Start () {
+		Shuffle(cards);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void Shuffle(GameObject[] cards){
+		for(int i=0; i<cards.Length; i++){
+			int rng = Random.Range(i, cards.Length);
+			GameObject tempCard = cards[i];
+			Debug.Log("i= "+i);
+			Debug.Log("rng= "+rng);
+			Debug.Log(cards[i].transform.localPosition);
+			Debug.Log(cards[rng].transform.localPosition);
+			cards[i]=cards[rng];
+			cards[i].transform.localPosition = cards[rng].transform.localPosition;
+			Debug.Log(cards[i].transform.localPosition);
+			cards[rng]=tempCard;
+			cards[rng].transform.localPosition=tempCard.transform.localPosition;	
+		}
+		
+	}
+}
